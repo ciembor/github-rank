324bec0a2464d06da57df9206763ff9f0be92ef2
ciembor <pub@ciemborowicz.pl>
2012-10-14 23:45:14 +0200

    organizing code

---
+++ config.py
@@ -0,0 +1,11 @@
+result_size = 15
+
+locations = set(
+  'polska': ['polska', 'poland'],
+  'warszawa': ['warszawa', 'warsaw'],
+  'krakow': ['krakow', 'kraków', 'cracow'],
+  'lodz': ['lodz', 'łódź'],
+  'wroclaw': ['wroclaw', 'wrocław'],
+  'poznan': ['poznan', 'poznań'],
+  'gdansk': ['gdansk', 'gdańsk'],
+)
--- main.py
+++ main.py
@@ -1,88 +1,12 @@
-import requests
-import json
-
-def getUsernames(locations):
-	usernames = set()
-	for location in locations:
-		r = requests.get('https://api.github.com/legacy/user/search/' + location)
-		if(r.ok):
-			result = json.loads(r.text or r.content)['users']
-			for user in result:
-				usernames.add(user['username'])
-	return usernames
-
-def getTop(usernames, n):
-	top = {'users': [], 'repositories': [{'watchers': 0}]}
-
-	for username in usernames:
-		stars_counter = 0
-
-		### get top repositories ###
-		r = requests.get('https://api.github.com/users/' + username + '/repos')
-		if(r.ok):
-			print(username)
-			result = json.loads(r.text or r.content)
-			for repository in result:
-				# count stars for users rank
-				stars_counter += int(repository['watchers'])
-				min_top = None 
-				to_append = None
-
-				for top_repository in top['repositories']:
-					if min_top is None or min_top > top_repository['watchers']:
-						min_top = top_repository['watchers']
-					if repository['watchers'] >= top_repository['watchers']:
-						to_append = repository
-
-				if to_append:
-					top['repositories'].append(to_append)
-						
-				# remove to low repositories
-				if min_top < repository['watchers']:
-					higher_count = 0
-					for repository in top['repositories']:
-						if repository['watchers'] > min_top:
-							higher_count += 1
-					if len(top['repositories']) > higher_count >= n:
-						for repository in top['repositories']:
-							if repository['watchers'] == min_top:
-								top['repositories'].remove(repository)
-		
-		### get top users ###
-		r = requests.get('https://api.github.com/users/' + username)
-		if (r.ok):
-			result = json.loads(r.text or r.content)
-			if ('User' == result['type']):
-				min_top_users = None
-				to_append = None
-				higher_count = 0
-	
-				result['stars'] = stars_counter
-	
-				if len(top['users']) < n:
-					top['users'].append(result)
-				else:
-					for top_user in top['users']:
-						if top_user['stars'] <= result['stars']:
-							to_append = result['stars']
-				
-				if to_append:
-					top['users'].append(result)
-	
-				for user in top['users']:
-					if min_top_users is None or min_top_users > user['stars']:
-						min_top_users = user['stars']
-	
-				if min_top_users < result['stars']:
-					higher_count = 0
-					for user in top['users']:
-						if user['stars'] > min_top_users:
-							higher_count += 1
-					if len(top['users']) > higher_count >= n:
-						for user in top['users']:
-							if user['stars'] == min_top_users:
-								top['users'].remove(user)	
-	return top
+try:
+  import config
+except Exception:
+  print("Can't load config.py")
+  sys.exit(1)
+try:
+  import auth
+except Exception:
+  print("Warning: can't load auth.py")
 
 locations = ['Krakow', 'Kraków', 'Cracow']
 usernames = getUsernames(locations)
